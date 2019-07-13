09b2a6286649127cebb5ba2dae381eef29421964
Thomas TAVERNIER <thomas@tavernier.dev>
2019-07-13 16:33:08 +0200

    1.2.0
    - local images supports

--- chrome/html/js/empty_new_tab_page.js
+++ chrome/html/js/empty_new_tab_page.js
@@ -1,7 +1,14 @@
-chrome.storage.sync.get(["bgColor"], function(get){
+chrome.storage.sync.get(["bgColor"], function (get) {
 	document.getElementById("setBackgroundStyle").style.backgroundColor = get.bgColor;
 });
 
-chrome.storage.sync.get(["bgImageUrl"], function(get){
-	document.getElementById("setBackgroundStyle").style.backgroundImage = "url(" + get.bgImageUrl + ")";
+chrome.storage.sync.get(["bgImageUrl"], function (get) {
+	var imageUrl = get.bgImageUrl;
+	if (imageUrl !== undefined) {
+		if (imageUrl.includes("http")) {
+			document.getElementById("setBackgroundStyle").style.backgroundImage = "url(" + imageUrl + ")";
+		} else {
+			document.getElementById("setBackgroundStyle").style.backgroundImage = "url(" + chrome.extension.getURL('/PUT_YOUR_IMAGE_HERE/' + imageUrl) + ")";
+		}
+	}
 });
--- chrome/popup/js/popup.js
+++ chrome/popup/js/popup.js
@@ -1,58 +1,65 @@
 // set color value of input
-setColor = function(){
-	chrome.storage.sync.set({"bgColor": document.getElementById("bgColor").value});
+function setColor() {
+	chrome.storage.sync.set({ "bgColor": document.getElementById("bgColor").value });
 	getColor();
 }
 
 // get color value of input
-getColor = function(){
-	chrome.storage.sync.get(["bgColor"], function(get){
-	if (get.bgColor != undefined) {
-		document.getElementById("bgColor").value = get.bgColor;
-		document.getElementById("preview").style.backgroundColor = get.bgColor;
-	}
-});
+function getColor() {
+	chrome.storage.sync.get(["bgColor"], function (get) {
+		if (get.bgColor != undefined) {
+			document.getElementById("bgColor").value = get.bgColor;
+			document.getElementById("preview").style.backgroundColor = get.bgColor;
+		}
+	});
 }
 
 // set image url value of input
-setUrl = function(){
+function setUrl() {
 	if (document.getElementById("bgImageUrl").value == "") {
-		chrome.storage.sync.set({"bgImageUrl": "/assets/noImage.png"});
+		chrome.storage.sync.set({ "bgImageUrl": "/assets/noImage.png" });
 	}
 	else {
-		chrome.storage.sync.set({"bgImageUrl": document.getElementById("bgImageUrl").value});
+		chrome.storage.sync.set({ "bgImageUrl": document.getElementById("bgImageUrl").value });
 	}
 	getUrl();
 }
 
 
 // get image url value of input
-getUrl = function(){
-	chrome.storage.sync.get(["bgImageUrl"], function(get){
-	if (get.bgImageUrl != undefined) {
-		if (get.bgImageUrl != "/assets/noImage.png") {
-			document.getElementById("bgImageUrl").value = get.bgImageUrl;
+function getUrl() {
+	chrome.storage.sync.get(["bgImageUrl"], function (get) {
+		var imageUrl = get.bgImageUrl;
+		if (imageUrl != undefined) {
+			if (imageUrl !== "/assets/noImage.png") {
+				document.getElementById("bgImageUrl").value = get.bgImageUrl;
+				if(imageUrl.includes("http")) {
+					document.getElementById("preview").src = get.bgImageUrl;
+				} else {
+					document.getElementById("preview").src = chrome.extension.getURL('/PUT_YOUR_IMAGES_HERE/' + imageUrl);
+				}
+			} else {
+				document.getElementById("preview").src = get.bgImageUrl;
+			}
 		}
-		document.getElementById("preview").src = get.bgImageUrl;
-	}
-});
+	});
 }
 
 // reset storage
-resetdefaults = function(){
+function resetdefaults() {
 	chrome.storage.sync.clear();
 	checkBgColor();
 	checkBgImageUrl();
-	
+
 }
 
-// launcher
-launcher = function(){
-getColor();
-document.getElementById("bgColor").addEventListener("input", setColor);
-getUrl();
-document.getElementById("bgImageUrl").addEventListener("input", setUrl);
+// init
+function init() {
+	getColor();
+	document.getElementById("bgColor").addEventListener("input", setColor);
+	getUrl();
+	document.getElementById("bgImageUrl").addEventListener("input", setUrl);
 }
 
 // launch script
-setTimeout(launcher);
+setTimeout(init);
